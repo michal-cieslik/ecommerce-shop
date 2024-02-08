a0dce51fab90d6fcc8c7afe7f4e44f8077b8a95d
michal-cieslik <michalcieslik105@gmail.com>
2024-02-08 22:54:52 +0100

    Create app DB context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ecommerce_shop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerce_shop.Data
+{
+    public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+    }
+
+}
