fe075ead70c8e348581b685d4152dd44d318c53a
michal-cieslik <michalcieslik105@gmail.com>
2024-02-08 22:58:27 +0100

    Modify project settings files

--- Program.cs
+++ Program.cs
@@ -1,20 +1,22 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+string connectionString = app.Configuration.GetConnectionString("DefaultConnection");
 
 app.UseHttpsRedirection();
 app.UseDefaultFiles();
