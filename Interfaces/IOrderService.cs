b1d8d25bbc300e0802dad7e73a3c66e15e040c66
michal-cieslik <michalcieslik105@gmail.com>
2024-02-09 19:54:30 +0100

    Create interfaces

---
+++ Interfaces/IOrderService.cs
@@ -0,0 +1,12 @@
+﻿using ecommerce_shop.Models;
+
+namespace ecommerce_shop.Services
+{
+    public interface IOrderService
+    {
+        Task<Order> AddOrderAsync(Order newOrder);
+        Task<List<Order>> GetOrdersAsync(int userId);
+        Task<Order> UpdateOrderAsync(int orderId, Order updatedOrder);
+        Task RemoveOrderAsync(int orderId);
+    }
+}
